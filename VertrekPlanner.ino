595fe1cc0c141949b0c2425ef1cfd9891ed99651
JosVanPutte <123905477+JosVanPutte@users.noreply.github.com>
2026-02-19 17:51:57 +0100

    changed oled for a 1.54 inch epaper display

--- VertrekPlanner.ino
+++ VertrekPlanner.ino
@@ -1,200 +1,217 @@
 #include <WiFi.h>
-
 #include <WiFiManager.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+#include <GxEPD2_BW.h>      // E-paper library
+#include <Fonts/FreeSansBold18pt7b.h> // Mooier font voor vertrektijd
+#include <Fonts/FreeSans9pt7b.h>     // Font voor kleine tekst
 #include "time.h"
 #include "storage.h"
 
-// Tijd instellingen
+// E-paper Pin Definities voor jouw C3 setup
+#define EPD_CS    0
+#define EPD_DC    1
+#define EPD_RST   2
+#define EPD_BUSY  5 // Het draadje naar pin 5
+#define EPD_SCL   4
+#define EPD_SDA   3
+
+// Initialiseer SSD1680 display (200x200)
+GxEPD2_BW<GxEPD2_154_D67, GxEPD2_154_D67::HEIGHT> display(GxEPD2_154_D67(EPD_CS, EPD_DC, EPD_RST, EPD_BUSY));
+
 const char* ntpServer = "pool.ntp.org";
-
-String urlEncode(const char *s);
-void voerUpdateUit();
-
-// Instellingen
 String scriptUrl; 
 char van_adres[64];
 char naar_adres[64];
 bool shouldSaveConfig = false;
- 
-// OLED Display instellingen
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-#define SDA_PIN 8
-#define SCL_PIN 9
-#define I2C_ADDR 0x3C
+#define INFO if (Serial) Serial.printf
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
-// De callback die wordt aangeroepen als de gebruiker op 'Save' klikt
-void saveConfigCallback() {
-  shouldSaveConfig = true;
-}
+void saveConfigCallback() { shouldSaveConfig = true; }
+
+// URL Encoder tool
 String urlEncode(const char *s) {
   String str = s;
   String encodedString = "";
-  char c;
   for (int i = 0; i < str.length(); i++) {
-    c = str.charAt(i);
-    if (isAlphaNumeric(c)) {
-      encodedString += c;
-    } else {
-      encodedString += '%';
-      encodedString += String(c, HEX);
-    }
+    char c = str.charAt(i);
+    if (isAlphaNumeric(c)) encodedString += c;
+    else { encodedString += '%'; encodedString += String(c, HEX); }
   }
   return encodedString;
 }
 
 void enterDeepSleep(uint64_t seconds) {
-  Serial.printf("Slaap voor %llu seconden...\n", seconds);
+  while (digitalRead(EPD_BUSY) == HIGH) {
+    delay(10);
+  }
+  INFO("Slaap voor %llu seconden...\n", seconds);
+  display.hibernate(); // Cruciaal voor e-paper levensduur!
   esp_sleep_enable_timer_wakeup(seconds * 1000000ULL);
   esp_deep_sleep_start();
+}
+// Functie om de datum-layout te tekenen
+void toonDatumLayout(struct tm timeinfo) {
+    INFO("toon datum\n");
+    display.setFullWindow();
+    display.firstPage();
+    do {
+        display.fillScreen(GxEPD_WHITE);
+        display.setTextColor(GxEPD_BLACK);
+        
+        // 1. Dag en Maand bovenin (bijv. "19 FEB")
+        display.setFont(&FreeSans9pt7b);
+        char bovenkant[15];
+        strftime(bovenkant, sizeof(bovenkant), "%d %b", &timeinfo);
+        display.setCursor(50, 30); // Beetje uitlijnen naar wens
+        display.print(bovenkant);
+
+        // 2. Dagnaam in het midden (bijv. "DON")
+        display.setFont(&FreeSansBold18pt7b); // Lekker groot font
+        char dagNaam[5];
+        strftime(dagNaam, sizeof(dagNaam), "%a", &timeinfo);
+        // Simpele centrering voor 200x200
+        display.setCursor(45, 110); 
+        display.print(dagNaam);
+        
+    } while (display.nextPage());
 }
 
 void setup() {
   Serial.begin(115200);
-  delay(1000);
+
+  pinMode(EPD_CS, OUTPUT);
+  pinMode(EPD_DC, OUTPUT);
+  pinMode(EPD_RST, OUTPUT);
+  pinMode(EPD_BUSY, INPUT);
+
+  INFO("Start op\n");
+  // 1. Initialiseer E-paper op C3 Hardware SPI
+  SPI.begin(EPD_SCL, -1, EPD_SDA, EPD_CS);
+  display.init(115200, true, 2, false);
+  display.setRotation(3); 
+
   nvs_handle_t nvs = initNvs();
   String savedVan = getNonVolatile(nvs, "van_param");
   String savedNaar = getNonVolatile(nvs, "naar_param");
   strncpy(van_adres, savedVan.c_str(), sizeof(van_adres));
   strncpy(naar_adres, savedNaar.c_str(), sizeof(naar_adres));
+
   WiFiManager wm;
   wm.setSaveConfigCallback(saveConfigCallback);
-  if (savedVan.isEmpty()) {
-    wm.resetSettings();
-  }
-  WiFiManagerParameter custom_van("van", "Vertrekadres", van_adres, 64);
+  if (savedVan.isEmpty()) wm.resetSettings();
+
+  WiFiManagerParameter custom_van("van", "Vertrek", van_adres, 64);
   WiFiManagerParameter custom_naar("naar", "Bestemming", naar_adres, 64);
-  
   wm.addParameter(&custom_van);
   wm.addParameter(&custom_naar);
 
-  // Start de portal
   if (!wm.autoConnect("VertrekTijdPlanner")) {
     delay(3000);
     ESP.restart();
   }
+
   if (shouldSaveConfig) {
     setNonVolatile(nvs, "van_param", custom_van.getValue());
     setNonVolatile(nvs, "naar_param", custom_naar.getValue());
   }
+
   String vanEncoded = urlEncode(custom_van.getValue());
   String naarEncoded = urlEncode(custom_naar.getValue());
   scriptUrl = "https://script.google.com/macros/s/AKfycbwDx259LxLCEk1udjipwHFzu6TmGXWtHFOgB17pChjpt1VowNhGMvsWk0sdbOh9Z3P8/exec?van=" + vanEncoded + "&naar=" + naarEncoded;
 
-  // 1. Scherm en I2C initialisatie
-  Wire.begin(SDA_PIN, SCL_PIN);
-  if(!display.begin(SSD1306_SWITCHCAPVCC, I2C_ADDR)) {
-    Serial.println(F("SSD1306 mislukt"));
-  }
-  display.clearDisplay();
-  display.setTextColor(WHITE);
-  
   configTime(0, 0, ntpServer);
-  setenv("TZ", "CET-1CEST,M3.5.0,M10.5.0/3", 1); // Nederland (Winter & Zomer)
+  setenv("TZ", "CET-1CEST,M3.5.0,M10.5.0/3", 1);
   tzset();
 
   struct tm timeinfo;
   if (!getLocalTime(&timeinfo)) {
-    Serial.println("Tijd ophalen mislukt");
     enterDeepSleep(60);
   }
 
-  // 3. Check Wakeup Cause
   esp_sleep_wakeup_cause_t wakeup_reason = esp_sleep_get_wakeup_cause();
 
+  // Beslis of we moeten updaten of slapen
   if (wakeup_reason != ESP_SLEEP_WAKEUP_TIMER) {
-    // --- SCENARIO A: HARD RESET (TEST MODE) ---
-    Serial.println("Handmatige reset: Toon data en slaap 1 minuut...");
-    voerUpdateUit(); 
-    delay(5000); 
-    enterDeepSleep(60); 
+    voerUpdateUit();
+    delay(5000);
+    enterDeepSleep(60);
   } else {
-    // --- SCENARIO B: WAKKER UIT TIMER ---
-    
-    // tm_wday: 2 = Dinsdag, 4 = Donderdag
-    bool isJuisteDag = (timeinfo.tm_wday == 2 || timeinfo.tm_wday == 4);
-    bool isVoorAchtUur = (timeinfo.tm_hour < 8);
-
-    if (isJuisteDag && isVoorAchtUur) {
-      // Het is een werkdag ochtend: Update het scherm
-      voerUpdateUit();
-      
-      // Optioneel: Slaap telkens 10 minuten om de reistijd te verversen tot 08:00
-      // Of laat hem gewoon aanstaan. Voor batterij is 10 min slaap beter:
-      Serial.println("Werkdag: Ververs over 3 minuten.");
-      enterDeepSleep(3 * 60); 
-    } else {
-      // Het is geen werkdag of het is al na 08:00: Slaap tot morgen 06:00
-      Serial.println("Geen actie nodig. Slaap tot morgen 06:00.");
-      display.ssd1306_command(SSD1306_DISPLAYOFF);
-      
-      int secondenNu = (timeinfo.tm_hour * 3600) + (timeinfo.tm_min * 60) + timeinfo.tm_sec;
-      int doelSeconden = 6 * 3600; // 06:00 uur
-      uint64_t slaapSeconden;
-
-      if (secondenNu < doelSeconden) {
-        slaapSeconden = doelSeconden - secondenNu;
-      } else {
-        slaapSeconden = (24 * 3600) - secondenNu + doelSeconden;
+    bool isJuisteDag = (timeinfo.tm_wday >= 1 && timeinfo.tm_wday <= 5); // Ma-Vr
+    if (isJuisteDag && timeinfo.tm_hour < 9) {
+      if (!voerUpdateUit()) {
+        toonDatumLayout(timeinfo);
       }
-      
-      enterDeepSleep(slaapSeconden);
+      enterDeepSleep(5 * 60); // Ververs elke 5 min tijdens de spits
+    } else {
+      toonDatumLayout(timeinfo);
+      // Slaap tot volgende ochtend 06:00
+      int secondenNu = (timeinfo.tm_hour * 3600) + (timeinfo.tm_min * 60);
+      int doel = 6 * 3600;
+      uint64_t slaap = (secondenNu < doel) ? (doel - secondenNu) : (86400 - secondenNu + doel);
+      enterDeepSleep(slaap);
     }
   }
 }
-void voerUpdateUit() {
-    Serial.println("executing script " + scriptUrl);
+
+bool voerUpdateUit() {
+    INFO("haal tijden op\n");
     HTTPClient http;
     http.setFollowRedirects(HTTPC_STRICT_FOLLOW_REDIRECTS);
     http.begin(scriptUrl);
     
     int httpCode = http.GET();
     if (httpCode > 0) {
+        INFO("Tijdsduur opgehaald\n");
         String payload = http.getString();
-        StaticJsonDocument<200> doc;
+        StaticJsonDocument<300> doc;
         deserializeJson(doc, payload);
 
-        const char* reistijdStr = doc["reistijd"]; // bijv "25 min"
-        const char* vertrekStr = doc["vertrek"];   // bijv "08:42"
+        const char* reistijdStr = doc["reistijd"]; 
+        const char* vertrekStr = doc["vertrek"];   
 
-        // 1. Haal getallen uit de strings
         int reistijdMinuten = atoi(reistijdStr); 
         int vertrekUur = atoi(vertrekStr);
-        int vertrekMin = atoi(vertrekStr + 3); // slaat "08:" over
+        int vertrekMin = atoi(vertrekStr + 3);
 
-        // 2. Bereken minuten tot vertrek
         struct tm timeinfo;
         getLocalTime(&timeinfo);
-        int nuInMinuten = (timeinfo.tm_hour * 60) + timeinfo.tm_min;
-        int vertrekInMinuten = (vertrekUur * 60) + vertrekMin;
-        int minutenOver = vertrekInMinuten - nuInMinuten;
+        int minutenOver = ((vertrekUur * 60) + vertrekMin) - ((timeinfo.tm_hour * 60) + timeinfo.tm_min);
+        INFO("Reistijd: %d min\n", reistijdMinuten);
+        INFO("Vertrek over %d min\n", minutenOver);
 
-        // 3. Display update
-        display.clearDisplay();
-        
-        // Bovenste regel: Tijd: [tt] over: [m]
-        display.setTextSize(2); // Groter lettertype (was 1)
-        display.setCursor(0, 0);
-        display.printf("T:%d ov:%d", reistijdMinuten, minutenOver);
+        // --- E-PAPER DRAWING ---
+        display.setFullWindow();
+        display.firstPage();
+        do {
+            display.fillScreen(GxEPD_WHITE);
+            
+            // Header bar
+            display.fillRect(0, 0, 200, 40, GxEPD_BLACK);
+            display.setFont(&FreeSans9pt7b);
+            display.setTextColor(GxEPD_WHITE);
+            display.setCursor(10, 25);
+            display.printf("Reistijd: %d min", reistijdMinuten);
 
-        display.drawLine(0, 18, 127, 18, WHITE); // Lijn iets lager wegens grotere tekst
+            // Grote vertrektijd in het midden
+            display.setFont(&FreeSansBold18pt7b);
+            display.setTextColor(GxEPD_BLACK);
+            display.setCursor(45, 110);
+            display.print(vertrekStr);
 
-        // Middelste regel: Vertrektijd
-        display.setTextSize(3);
-        display.setCursor(19, 30); 
-        display.print(vertrekStr);
+            // Onderste regel
+            display.setFont(&FreeSans9pt7b);
+            display.setCursor(35, 160);
+            if(minutenOver > 0) {
+              display.printf("Vertrek over %d min", minutenOver);
+            }
+            else display.print("Nu vertrekken!");
 
-        display.display();
+        } while (display.nextPage());
+    } else {
+      INFO("ERROR getting tijdsduur %d\n", httpCode);
     }
     http.end();
-    delay(2000);
+    return httpCode > 0;
 }
-void loop() {
-  delay(60000);
-}
+
+void loop() {}
